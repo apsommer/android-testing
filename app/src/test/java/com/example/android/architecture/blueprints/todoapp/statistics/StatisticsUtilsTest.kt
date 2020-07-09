7862dd7223cbb02722b29b2924793713ffbe8f0d
apsommer <drew@sommerengineering.com>
2020-07-08 21:10:56 -0700

    test: Use Hamcrest matchers

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -1,11 +1,14 @@
 package com.example.android.architecture.blueprints.todoapp.statistics
 
 import com.example.android.architecture.blueprints.todoapp.data.Task
+import org.hamcrest.Matchers.`is`
 import org.junit.Assert.*
 import org.junit.Test
 
+// since no runner is specified the system default JUnitRunner is used
 class StatisticsUtilsTest {
 
+    // naming convention is subjectUnderTest_actionOrInput_resultState
     @Test
     fun getActiveAndCompletedStats_noCompleted_returnsHundredZero() {
 
@@ -15,8 +18,12 @@ class StatisticsUtilsTest {
         // call your function
         val result = getActiveAndCompletedStats(tasks)
 
-        // check the result
-        assertEquals(result.completedTasksPercent, 0f)
-        assertEquals(result.activeTasksPercent, 100f)
+        // check the result using Truth (Google code base)
+//        assertEquals(result.completedTasksPercent, 0f)
+//        assertEquals(result.activeTasksPercent, 100f)
+
+        // check the result using Hamcrest matcher
+        assertThat(result.activeTasksPercent, `is`(100f))
+        assertThat(result.completedTasksPercent, `is`(0f))
     }
 }
